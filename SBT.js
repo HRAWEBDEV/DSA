400775bac524c01196ada14b90defc8b78b1db31
hamidwise1 <hrawebdev@gmail.com>
2024-08-09 15:36:18 +0330

    add min max to binary serach tree

--- SBT.js
+++ SBT.js
@@ -18,6 +18,7 @@ class SBT {
    this.root = newNode;
   } else {
    while (temp) {
+    if (temp.value == newNode.value) return undefined;
     const direction = temp.value < newNode.value ? 'right' : 'left';
     const directionNode = temp[direction];
     if (directionNode) {
@@ -33,6 +34,46 @@ class SBT {
   }
   return this;
  }
+ contain(value) {
+  if (!this.length) return false;
+  let temp = this.root;
+  while (temp) {
+   if (temp.value == value) return true;
+   const direction = temp.value < value ? 'right' : 'left';
+   const directionNode = temp[direction];
+   if (directionNode) {
+    temp = directionNode;
+    continue;
+   } else {
+    return false;
+   }
+  }
+  return false;
+ }
+ min(root) {
+  const direction = 'left';
+  let temp = root ? root : this.root;
+  let minValue = temp;
+  if (!temp) return minValue;
+  while (temp) {
+   temp = temp[direction];
+   if (!temp) break;
+   minValue = temp;
+  }
+  return minValue;
+ }
+ max(root) {
+  const direction = 'right';
+  let temp = root ? root : this.root;
+  let maxValue = temp;
+  if (!temp) return maxValue;
+  while (temp) {
+   temp = temp[direction];
+   if (!temp) break;
+   maxValue = temp;
+  }
+  return maxValue;
+ }
 }
 
 const testTree = new SBT();
@@ -41,4 +82,9 @@ testTree.insert(12);
 testTree.insert(14);
 testTree.insert(8);
 testTree.insert(7);
-console.log(testTree);
+testTree.insert(5);
+testTree.insert(2);
+testTree.insert(22);
+
+console.log(testTree.min());
+console.log(testTree.max());
